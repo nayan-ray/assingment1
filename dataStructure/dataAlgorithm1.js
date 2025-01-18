c4b4c5dfdc2bc2160731eb6b6dee94118b532e38
nayan-ray <niranzanroy1@gmail.com>
2025-01-18 12:58:26 +0600

    v1

--- dataStructure/dataAlgorithm1.js
+++ dataStructure/dataAlgorithm1.js
@@ -1,7 +1,7 @@
 //Question 1: counting character from the string 
 //Question 2: lowest and height value from and array and remove unnecessary value
-//Question 1: corresponding value from the arrays(compare value of two arrays),method 1,2.
-  
+//Question 3: corresponding value from the arrays(compare value of two arrays),method 1,2.
+//Question 4: recursion [sum of element of an array]
   
   //........1
   // counting character from the string
@@ -70,4 +70,41 @@ function characterCount (str) {
  
 //Question 3: corresponding value from the arrays(compare value of two arrays),method 1,2.
   //method 1
-  console.log(5 ** 3)
+  // console.log(5 ** 3)
+
+
+//Question 4: recursion [sum of element of an array]
+
+function sumOfArray(arr){
+  if(arr.length === 0){
+    return 0;
+  }
+
+ let sum;
+//  for(let i = 0; i < arr.length; i++){
+//      sum = sum + arr[i];
+//  }
+
+sum = arr[0] + sumOfArray(arr.slice(1));
+    //2+3 
+
+
+  
+  return sum;
+}
+
+// console.log(sumOfArray([2, 3, 5, 5]))
+
+
+function findOddNumber(arr) {
+    let oddNumber = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 !== 0) {
+        oddNumber.push(arr[i]);
+        }
+      }
+      return oddNumber;
+}
+
+
+console.log(findOddNumber([5,2,6,7,8,5]))
